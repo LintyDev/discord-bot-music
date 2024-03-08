ad264fae8b2fe72ec10a69d8870fadbbf1c922f7
Linty <lintydev@gmail.com>
2024-03-08 17:33:32 +0100

    add new check for command

--- utils/checks.py
+++ utils/checks.py
@@ -4,10 +4,20 @@ from utils.errors import BotError
 
 def check_bot_channel(ctx: commands.Context) -> bool:
     if ctx.channel.name != "lintymusic-bot":
-        raise BotError(f"{ctx.bot.user.name} can't go here")
+        raise BotError(f"{ctx.bot.user.name} can't go here - Type in #lintymusic_bot")
     return True
 
 def check_user_channel(ctx: commands.Context) -> bool:
     if ctx.author.voice is None or ctx.author.voice.channel is None:
         raise BotError("You must be in a voice channel before issuing this command")
+    return True
+
+def check_bot_is_playing(ctx: commands.Context) -> bool:
+    if ctx.voice_client.is_playing():
+        raise BotError(f"{ctx.bot.user.name} is already playing music !")
+    return True
+
+def check_bot_is_not_playing(ctx: commands.Context) -> bool:
+    if not ctx.voice_client.is_playing():
+        raise BotError(f"{ctx.bot.user.name} does not play music !")
     return True
